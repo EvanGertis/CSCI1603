0688cf27d45c0d7a0354b050d3fa961e2f54d3b5
Evan Gertis <physevangertis@gmail.com>
2022-03-28 11:16:41 -0400

    adding make stars recursive function

---
+++ Recursion/Test8.java
@@ -0,0 +1,32 @@
+/* 
+author: Evan Gertis
+date: 03/28/2022
+*/
+package Recursion;
+
+public class Test8 {
+    public static void main(String args[]) {
+        System.out.println(makeStars(0));
+        System.out.println(makeStars(8));
+    }
+
+    public static String makeStars(int n){
+        //base case 1
+        if(n == 0)
+            return "";
+        //base case 2
+        if(n == 1)
+            return "*";
+        return '*' + makeStars(n-1);
+    }
+}
+
+// ********
+// *
+// makeStars(n - 1)
+// * + *
+// makeStars(n - 1) + makeStars(n - 2) 
+// * + * + *
+// * + * + * + *
+// * + * + * + * + *
+// * + * + * + * + * + *
