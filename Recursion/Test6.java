b9b3c9cca25ab7fa5831567eb98de2daa0a065d8
Evan Gertis <physevangertis@gmail.com>
2022-03-28 11:15:44 -0400

    add even and odd factorial recursion method

---
+++ Recursion/Test6.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Test6 {
+    public static void main(String[] args){
+        System.out.println(oddevenfact(6));
+    }
+
+    public static long oddevenfact(int n){
+        if(n<=0)
+            return 1;
+        return n * oddevenfact(n-2);
+    }
+}
