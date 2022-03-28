65859ec4bb71f20d07bca3e89247411d362a8b78
Evan Gertis <physevangertis@gmail.com>
2022-03-28 11:41:15 -0400

    adding a recursive method for clearing the elements of an array

---
+++ Recursion/Test10.java
@@ -0,0 +1,37 @@
+/* 
+author: Evan Gertis
+date: 03/28/2022
+*/
+package Recursion;
+
+public class Test10 {
+    public static void main(String args[]){
+        int[] test = new int[]{1,3,5,3,8,2};
+        for(int i = 0; i < test.length; i++){
+            System.out.print(test[i]);
+        }
+        System.out.print('\n');
+        clear(test, test.length);
+        for(int i = 0; i < test.length; i++){
+            System.out.print(test[i]);
+        }
+        System.out.print('\n');
+    }
+    //input: int array, int n elements
+    //output: set array values to 0 by value
+    public static void clear(int[] array, int n){
+        if(n == 0)
+            return;
+        array[n-1] = 0;
+        clear(array, n -1);
+    }
+}
+
+// Example
+// [1,3,5,3,8,2], n = 6
+// [0,3,5,3,8,2], n = 5
+// [0,0,5,3,8,2], n = 4
+// [0,0,0,3,8,2], n = 3
+// [0,0,0,0,8,2], n = 2
+// [0,0,0,0,0,2], n = 1
+// [0,0,0,0,0,0], n = 0
