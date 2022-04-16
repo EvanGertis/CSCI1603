60871a7eb05e8e02751d6be698fcbbebb5c29cbe
Evan Gertis <physevangertis@gmail.com>
2022-04-16 11:36:49 -0400

    adding notes and slides

--- Recursion/printModule.java
+++ Recursion/printModule.java
@@ -1,19 +1,20 @@
 package Recursion;
-/*
+/* 
 Author: Evan Gertis
-Date: 04/16 
-recursive print module
+Date: 04/16
+print module
+output: 1 2 3 4 5
 */
 
-public class printModule {
+public class printReverse {
     public static void main(String[] args){
         xMethod(5);
     }
 
     public static void xMethod(int n){
         if(n > 0){
-            System.out.print(n+" ");
             xMethod(n-1);
+            System.out.print(n+" ");
         }
     }
 }
