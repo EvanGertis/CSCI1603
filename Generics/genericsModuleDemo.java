cfb47e483c36929c1658fbd7e1378f7283de913c
Evan Gertis <physevangertis@gmail.com>
2022-04-18 09:54:15 -0400

    add genericsModuleDemo.java

---
+++ Generics/genericsModuleDemo.java
@@ -0,0 +1,17 @@
+package Generics;
+
+public class genericsModuleDemo {
+    public static void main(String[] args){
+        Integer[] integers = {1,2,3,4,5};
+        String[]  strings  = {"London", "Paris", "New York","Austin"};
+
+        genericsModuleDemo.<Integer>print(integers);
+        genericsModuleDemo.<String>print(strings);
+    }
+
+    public static <E> void print(E[] list){
+        for(int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+        System.out.println();
+    }
+}
