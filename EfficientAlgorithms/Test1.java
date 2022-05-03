aebc8a971830f5d5f5e0137a1961c02fb7bc899b
Evan Gertis <physevangertis@gmail.com>
2022-05-03 15:12:50 -0400

    add sorting materials

--- EfficientAlgorithms/Test1.java
+++ EfficientAlgorithms/Test1.java
@@ -1,24 +1,37 @@
 import java.util.*;
 
-/*
-    name: Evan Getis
-    date: 04/02
-    program: maximum consecutive increasingly ordered substring
-*/
 public class Test1 {
-    
-    //input:Enter a string: abcabcdgabxy
-    //output:Maximum consecutive increasingly ordered substring is abcdg
-    public static void main(String args[]){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String s    = input.nextLine();
-        System.out.printf("Maximum consecutive increasingly ordered substring is %s",maxConsecutiveSubstring(s));
-    }
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		LinkedList<Character> max = new LinkedList<>();
+		LinkedList<Character> list = new LinkedList<>();
 
-    public static String maxConsecutiveSubstring(String s){
+		// Prompt the user to enter a string
+		System.out.print("Enter a string: ");
+		String string = input.nextLine(); 
 
-        return s;
-    }
+		// Find the maximum consecutive increasingly ordered substring
+		for (int i = 0; i < string.length(); i++) { 		// single loop
+			if (list.size() > 1 && string.charAt(i) <= list.getLast() && 
+				list.contains(string.charAt(i))) {
+				list.clear(); // Simple statement
+			}
+            System.out.print(list+"\n");
+
+			list.add(string.charAt(i)); // Simple statement
+
+			if (list.size() > max.size()) { // Simple statement
+				max.clear();
+				max.addAll(list);
+			}
+		}
+
+		// Display the maximum consecutive 
+		// increasingly ordered substring
+		for (Character ch: max) { // single loop
+			System.out.print(ch); // Simple statement
+		}
+		System.out.println();
+	}
 }
-
